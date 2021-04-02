6c3747cb777dc09832190870affd01d33a035c41
Togglebit <me@togglebit.io>
2021-04-02 14:27:08 +0200

    include the horrible one

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,40 @@ use nightmaregl::events::{Event, Key as WinitKey, KeyState, LoopAction};
 use nightmaregl::{Color, Context, Position, Result, Size, Animation, Sprite, Renderer, Viewport};
 use nightmaregl::texture::{Wrap, Texture};
 
-mod canvas;
-mod commands;
+// mod canvas;
+// mod commands;
 mod input;
 
 use input::{Input, Key};
-use canvas::Canvas;
-use commands::CommandInput;
+// use canvas::Canvas;
+// use commands::CommandInput;
 
 fn run() -> Result<()> {
     let (eventloop, mut context) = Context::builder("Mixel: the modal pixel editor")
         .vsync(true)
+        .resizable(false)
+        .with_size(Size::new(601, 333))
         .build()?;
 
     let window_size = context.window_size::<i32>();
 
-    // -----------------------------------------------------------------------------
-    //     - Canvas -
-    // -----------------------------------------------------------------------------
-    let mut canvas = Canvas::new(
-        window_size,
-        Size::new(32, 32),
-        Color {
-            b: 0.5,
-            ..Default::default()
-        },
-        &mut context,
-    )?;
+    // // -----------------------------------------------------------------------------
+    // //     - Canvas -
+    // // -----------------------------------------------------------------------------
+    // let mut canvas = Canvas::new(
+    //     window_size,
+    //     Size::new(32, 32),
+    //     Color {
+    //         b: 0.5,
+    //         ..Default::default()
+    //     },
+    //     &mut context,
+    // )?;
 
-    // -----------------------------------------------------------------------------
-    //     - Command input -
-    // -----------------------------------------------------------------------------
-    let mut commands = CommandInput::new(&mut context)?;
+    // // -----------------------------------------------------------------------------
+    // //     - Command input -
+    // // -----------------------------------------------------------------------------
+    // let mut commands = CommandInput::new(&mut context)?;
 
     // -----------------------------------------------------------------------------
     //     - Input -
@@ -45,20 +47,24 @@ fn run() -> Result<()> {
     // -----------------------------------------------------------------------------
     let viewport = Viewport::new(Position::zero(), context.window_size());
     let mut renderer = Renderer::default(&mut context)?;
-    renderer.pixel_size *= 4.0;
+    renderer.pixel_size *= 1.0;
 
-    let texture = Texture::<f32>::from_disk("src/horrid.png")?;
+    let texture = Texture::<f32>::from_disk("src/horrible.png")?;
     texture.wrap_x(Wrap::NoWrap);
     texture.wrap_y(Wrap::NoWrap);
     let sprite = {
         let mut s = Sprite::<f32>::new(texture.size());
-        s.size = Size::new(17.0, 13.0);
+        // s.size = Size::new(16.0, 12.0);
+        // s.texture_offset= Position::new(0.0, 0.0);
+
+        s.size = Size::new(13.0, 7.0);
+        s.texture_offset= Position::new(16.0, 3.0);
         s
     };
 
-    let mut animation = Animation::new(sprite, 2, 2, 32);
-    animation.should_loop = false;
-    animation.fps = 0.0;
+    // let mut animation = Animation::new(sprite, 2, 2, 32);
+    // animation.should_loop = false;
+    // animation.fps = 0.0;
 
     let mut counter = 0;
 
@@ -69,7 +75,7 @@ fn run() -> Result<()> {
         match event {
             Event::Char(c) => {
                 input.update(Key::Char(c));
-                commands.input(&mut input);
+                // commands.input(&mut input);
             }
             Event::KeyInput {
                 key,
@@ -77,7 +83,7 @@ fn run() -> Result<()> {
             } => {
                 input.update(Key::Key(key));
                 // canvas.input(&input);
-                commands.input(&mut input);
+                // commands.input(&mut input);
             }
 
             Event::Draw(dt) => {
@@ -85,14 +91,19 @@ fn run() -> Result<()> {
                 // canvas.render(&mut context);
                 // commands.render(&mut context);
 
-                renderer.render(
+                let res = renderer.render(
                     &texture,
-                    &vec![animation.sprite.vertex_data()],
+                    &vec![sprite.vertex_data()],
                     &viewport,
                     &mut context
                 );
+
+                if let Err(e) = res {
+                    eprintln!("failed to render: {:?}", e);
+                }
+
                 context.swap_buffers();
-                animation.update(dt);
+                // animation.update(dt);
             }
 
             Event::Resize(_new_size) => {}
